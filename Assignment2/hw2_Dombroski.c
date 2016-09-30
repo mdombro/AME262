303ea692a8761457d421a96d817d709b038c9972
Matthew <mdombro3@gmail.com>
2016-09-29 22:10:04 -0400

    v0.1 done

--- Assignment2/hw2_Dombroski.c
+++ Assignment2/hw2_Dombroski.c
@@ -4,14 +4,14 @@
 #define BLOCK 64
 
 typedef struct {
-  char note[2];
+  char note[5];
   int duration;
 } NOTE;
 
 char *parseNote(int n);
 
 int main (int argc, char[] argv) {
-  char notes[][] = {'1', '2', '3', '4', '5', '6', '7', 'i^'};  // available notes on the C major scale
+  //char notes[][] = {'1', '2', '3', '4', '5', '6', '7', 'i^'};  // available notes on the C major scale
   time_t t;  // variable to hold system time that will seed the random number gen
   int voiceLeadingFlag = 1;
   int melodyCounter = 0;
@@ -19,6 +19,8 @@ int main (int argc, char[] argv) {
   int n = 1; // multipicator for melodySize
   int note;
   int dur;
+  int beatCount = 0;
+  int measureCount = 0;
   srand((unsigned) time(&t));  // seed rand
 
   // declare block of struct pointers
@@ -26,7 +28,7 @@ int main (int argc, char[] argv) {
 
   // allocate first C note at beginning of Melody
   Melody[0] = malloc(sizeof(NOTE));
-  Melody[0]->note = 'C';
+  Melody[0]->note = "1  ";
   Melody[0]->duration = (rand()%(3-1))+1;
 
   // loop executes until a D-C or B-c transition occurs
@@ -40,14 +42,26 @@ int main (int argc, char[] argv) {
     note = (rand()%(8-1))+1;
     // Check for avoid conditions
     // check for leading voice conditions
+    if ((strcmp(Melody[melodyCounter-1]->note, "2  ") == 0 && note == 1) || (strcmp(Melody[melodyCounter-1]->note, "7  ") == 0 && note == 8)) {
+      voiceLeadingFlag = 0;
+    }
+
     Melody[melodyCounter] = malloc(sizeof(NOTE));
     Melody[melodyCounter]->note = parseNote(note);
     Melody[melodyCounter]->duration = (rand()%(3-1))+1;
   }
 
-  // print out the melody 
+  // print out the melody
   for (int i = 0; i < melodyCounter; i++) {
-
+    printf("%s", Melody[melodyCounter]->note); beatCount++;
+    if (beatCount == 3) {printf("|  "); beatCount = 0; measureCount++;}
+    if (measureCount == 3) {printf("\n"); measureCount = 0;}
+    dur = Melody[melodyCounter]->duration;
+    while (--dur > 0) {  // pre-decrement so a one beat note has no '-  '
+      printf("-  "); beatCount++;
+      if (beatCount == 3) {printf("|  "); beatCount = 0; measureCount++;}
+      if (measureCount == 3) {printf("\n"); measureCount = 0;}
+    }
   }
 }
 
@@ -55,28 +69,28 @@ char *parseNote(int n) {
   char *note;
   switch (n) {
     case 1:
-      *note = "1";
+      *note = "1  ";
       break;
     case 2:
-      *note = "2";
+      *note = "2  ";
       break;
     case 3:
-      *note = "3";
+      *note = "3  ";
       break;
     case 4:
-      *note = "4";
+      *note = "4  ";
       break;
     case 5:
-      *note = "5";
+      *note = "5  ";
       break;
     case 6:
-      *note = "6";
+      *note = "6  ";
       break;
     case 7:
-      *note = "7";
+      *note = "7  ";
       break;
     case 8:
-      *note = "1^";
+      *note = "1^ ";
       break;
     default:
       break;
